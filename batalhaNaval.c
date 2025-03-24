e87ba4ebf941b43d77d5277fe06df1c38ea2256b
WesleyA0101 <wesleyempresarial0101@gmail.com>
2025-03-24 19:22:55 -0300

    nivel_aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <locale.h>
 
-// Nivel novato
+// Nivel aventureiro
 
 #define TAMANHO 10
 #define NAVIO_TAMANHO 3
+#define NAVIOS 4
 
 void inicializartabuleiro(int tabuleiro[TAMANHO][TAMANHO]){
     for(int i = 0; i < TAMANHO; i++){
@@ -17,26 +18,42 @@ void inicializartabuleiro(int tabuleiro[TAMANHO][TAMANHO]){
 
 }
 
-void posicionarnavio(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, char orientacao){
-    if(orientacao == 'H' && coluna + NAVIO_TAMANHO <= TAMANHO){
-        for(int i = 0; i < NAVIO_TAMANHO; i++){
-	    tabuleiro[linha][coluna + i] = 3;
+
+int podeposicionar(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcaolinha, int direcaocoluna){
+
+    for(int i = 0; i < NAVIO_TAMANHO; i++){
+        int novalinha = linha + (i * direcaolinha);
+	int novacoluna = coluna + (i * direcaocoluna);
+
+	if(novalinha < 0 || novalinha >= TAMANHO || novacoluna < 0 || novacoluna >= TAMANHO){
+	    return 0;
+	
 	}
-    }else if(orientacao == 'V' && linha + NAVIO_TAMANHO <= TAMANHO){
-        for(int i = 0; i < NAVIO_TAMANHO; i++){
-	    tabuleiro[linha + i][coluna] = 3;
+	if(tabuleiro[novalinha][novacoluna] != 0){
+	    return 0;
 	}
     }
+    return 1;
+
 
 }
 
+void posicionarnavio(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, int direcaolinha, int direcaocoluna){
+    for(int i = 0; i < NAVIO_TAMANHO; i++){
+        tabuleiro[linha + (i * direcaolinha)][coluna + (i * direcaocoluna)] = 3;
+    
+    }
+}
+
 void exibirtabuleiro(int tabuleiro[TAMANHO][TAMANHO]){
+    printf("\nTabuleiro\n");
     for(int i = 0; i < TAMANHO; i++){
         for(int j = 0; j < TAMANHO; j++){
-	    printf("%d ", tabuleiro[i][j]);
+	    printf("%d", tabuleiro[i][j]);
 	}
 	printf("\n");
     }
+
 }
 
 int main() {
@@ -45,15 +62,29 @@ int main() {
     int tabuleiro[TAMANHO][TAMANHO];
     inicializartabuleiro(tabuleiro);
 
-    //Coordenadas 
-    int linhaH = 2, colunaH = 4;
-    int linhaV = 5, colunaV = 7;
+    int navios[4][4] = {
+        {2, 4, 0, 1},
+	{5, 7, 1, 0},
+	{1, 1, 1, 1},
+	{2, 8, 1, -1}
+    
+    };
 
-    //Posição do navio
-    posicionarnavio(tabuleiro, linhaH, colunaH, 'H');
-    posicionarnavio(tabuleiro, linhaV, colunaV, 'V');
+    for(int i = 0; i < NAVIOS; i++){
+        int linha = navios[i][0];
+	int coluna = navios[i][1];
+	int direcaolinha = navios[i][2];
+	int direcaocoluna = navios[i][3];
 
-    //Exibe o tabuleiro
+	if(podeposicionar(tabuleiro, linha, coluna, direcaolinha, direcaocoluna)){
+	    posicionarnavio(tabuleiro, linha, coluna, direcaolinha, direcaocoluna);
+	
+	}else{
+	    printf("Erro ao posicionar o navio %d. Coordenadas inválidas!\n", i + 1);
+	}
+    
+    }
+
     exibirtabuleiro(tabuleiro);
 
     return 0;
